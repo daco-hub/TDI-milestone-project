e736bd84d15ef56bceabc4bf215232cd945de33b
Davide Cortesi <80471263+daco-hub@users.noreply.github.com>
2021-09-20 23:36:05 +0200

    Added dropdown menu

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ def load_data(ticker):
     r = requests.get(url)
     data = r.json()
 
-    df = pd.DataFrame(data['Time Series (Daily)']).T[['1. open', '5. adjusted close']]
+    df = pd.DataFrame(data['Time Series (Daily)']).T[['1. open', '2. high', '3. low', '4. close', '5. adjusted close']]
     df.index = pd.to_datetime(df.index)
     df.index.name = 'Date'
     df.sort_index(inplace=True)
-    df.rename(columns={'1. open': 'open', '5. adjusted close': 'close'}, inplace=True)
+    df.rename(columns={'1. open': 'open', '2. high': 'day high' , '3. low': 'day low', 
+    '4. close': 'close', '5. adjusted close': 'adjusted close'}, inplace=True)
     return df
 
 
-def get_graph(df, ticker):
+def get_graph(df, ticker, selection):
     source = ColumnDataSource(df)
-    selection = 'close'
 
     p = figure(x_axis_type='datetime', x_axis_label='Date', title='Stock price: ' + ticker,
                plot_width=800, plot_height=600)
@@ -43,9 +43,10 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         ticker_content = request.form['ticker_input']
+        selection = request.form['selection']
         ticker_df = load_data(ticker_content)
         
-        graph = get_graph(ticker_df, ticker_content)
+        graph = get_graph(ticker_df, ticker_content, selection)
 
         script, div = components(graph)
         return render_template("graph.html", the_div=div, the_script=script)
